702b40f1a6dbe4e4efd4f52ad0b08954f3af6182
Sush25b <phulsundarsushant26@gmail.com>
2019-10-11 10:53:55 +0530

    [Sushant] Add. capability to evolve pattern when three alive

--- src/main/java/com/thoughtworks/game/GameOfLife.java
+++ src/main/java/com/thoughtworks/game/GameOfLife.java
@@ -2,13 +2,28 @@ package com.thoughtworks.game;
 
 public class GameOfLife {
 
+    int[][] initialCluster;
     int[][] evolvePattern;
 
     public GameOfLife(int[][] initialCluster) {
+        this.initialCluster = initialCluster;
     }
 
     public int[][] evolvePattern() {
-        evolvePattern = new int[][]{{0, 0}, {0, 0,}};
+
+        int totalAlive = 0;
+        for (int row = 0; row < initialCluster.length; row++) {
+            for (int column = 0; column < initialCluster[row].length; column++) {
+                    if(initialCluster[row][column]==1)
+                        ++totalAlive;
+            }
+        }
+
+        if(totalAlive<=2) {
+            evolvePattern = new int[][]{{0, 0}, {0, 0,}};
+            return evolvePattern;
+        }
+        evolvePattern= new int[][]{{1,1},{1,1}};
         return evolvePattern;
     }
 }
--- src/test/java/com/thoughtworks/game/GameOFLifeTest.java
+++ src/test/java/com/thoughtworks/game/GameOFLifeTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 public class GameOFLifeTest {
     @Test
-    public void givenClusterWithAllDead_whenEvolve_thenReturnClusterOFAllDead() {
+    public void givenClusterWithAllDead_whenEvolve_thenReturnClusterOFAllDead() {    //0 alive
         int[][] initialCluster = {{0, 0}, {0, 0}};
         GameOfLife gameOfLife = new GameOfLife(initialCluster);
 
@@ -15,7 +15,7 @@ public class GameOFLifeTest {
     }
 
     @Test
-    public void givenClusterWithTwoAlive_whenEvolve_thenReturnClusterOFAllDead() {
+    public void givenClusterWithTwoAlive_whenEvolve_thenReturnClusterOFAllDead() {    //2 alive
         int[][] initialCluster = {{0, 0}, {1, 1}};
         GameOfLife gameOfLife = new GameOfLife(initialCluster);
 
@@ -23,4 +23,15 @@ public class GameOFLifeTest {
 
         assertArrayEquals(evolvedCluster, gameOfLife.evolvePattern());
     }
+
+    @Test
+    public void givenClusterWithThreeAlive_whenEvolve_thenReturnClusterOFAllDead() {   //3 alive
+        int[][] initialCluster = {{0, 1}, {1, 1}};
+        GameOfLife gameOfLife = new GameOfLife(initialCluster);
+
+        int[][] evolvedCluster = {{1, 1}, {1, 1}};
+
+        assertArrayEquals(evolvedCluster, gameOfLife.evolvePattern());
+    }
+
 }
