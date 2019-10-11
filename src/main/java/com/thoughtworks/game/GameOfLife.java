42c7e406f0e40c540a5c1c33a918a0cede98991b
Sush25b <phulsundarsushant26@gmail.com>
2019-10-11 10:29:18 +0530

    [Sushant] Add. result is taken as a constant

---
+++ src/main/java/com/thoughtworks/game/GameOfLife.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.game;
+
+public class GameOfLife {
+    public GameOfLife(int[][] initialCluster)
+    {
+    }
+
+    public int[][] evolvePattern()
+    {
+        return new int[][]{{0, 0},{0,0,}};
+    }
+}
---
+++ src/test/java/com/thoughtworks/game/GameOFLifeTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.game;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameOFLifeTest {
+    @Test
+    public void givenClusterWithAllDead_whenEvolve_thenReturnClusterOFAllDead() {
+        int[][] initialCluster = {{0, 0}, {0, 0}};
+        GameOfLife gameOfLife = new GameOfLife(initialCluster);
+
+        int[][] evolvedCluster = {{0, 0}, {0, 0}};
+
+        assertArrayEquals(evolvedCluster, gameOfLife.evolvePattern());
+    }
+}
