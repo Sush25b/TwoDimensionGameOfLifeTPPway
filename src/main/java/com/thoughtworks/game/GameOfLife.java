384559d78d63f2ef449273e2dcc43632b478a5f2
Sush25b <phulsundarsushant26@gmail.com>
2019-10-11 10:34:20 +0530

    [Sushant] Add. variable assign for the result value

--- src/main/java/com/thoughtworks/game/GameOfLife.java
+++ src/main/java/com/thoughtworks/game/GameOfLife.java
@@ -1,12 +1,14 @@
 package com.thoughtworks.game;
 
 public class GameOfLife {
-    public GameOfLife(int[][] initialCluster)
-    {
+
+    int[][] evolvePattern;
+
+    public GameOfLife(int[][] initialCluster) {
     }
 
-    public int[][] evolvePattern()
-    {
-        return new int[][]{{0, 0},{0,0,}};
+    public int[][] evolvePattern() {
+        evolvePattern = new int[][]{{0, 0}, {0, 0,}};
+        return evolvePattern;
     }
 }
